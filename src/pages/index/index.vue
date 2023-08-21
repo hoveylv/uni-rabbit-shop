de21648b8f855a4af5de78d9d09cbf70da25071b
hoveylv <hovey.lv@qq.com>
2023-08-21 18:20:41 +0800

    feat: 通用轮播组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
+import type { BannerItem } from '@/types/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { getHomeBannerAPI } from '@/services/home'
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
 
-//
+// 获取轮播图数据
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+
+// 页面加载
+onLoad(async () => {
+  await Promise.all([getHomeBannerData()])
+})
 </script>
 
 <template>
   <CustomNavbar />
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -1,12 +1,9 @@
-/**
- * declare module '@vue/runtime-core'
- *   现调整为
- * declare module 'vue'
- */
-import 'vue'
+import XtxSwiper from '@/components/XtxSwiper.vue'
 
 declare module 'vue' {
   export interface GlobalComponents {
-    //
+    XtxSwiper: typeof XtxSwiper
   }
 }
+
+export type XtxSwiperInstance = InstanceType<typeof XtxSwiper>
